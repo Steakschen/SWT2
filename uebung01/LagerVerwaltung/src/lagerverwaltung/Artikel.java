1255060c05e01c9735b4f96df2ebe894b974cd1a
Carsten Gross <carstengross1986@googlemail.com>
2014-05-15 15:59:05 +0200

    NetBeans angelegt
    klassen erstellt
    Header erstellt

---
+++ uebung01/LagerVerwaltung/src/lagerverwaltung/Artikel.java
@@ -0,0 +1,16 @@
+/**
+ * Softwaretechnik 2 
+ * Übung 1
+ * @author Carsten Gross / Moritz Fey
+ */
+
+package lagerverwaltung;
+//import de.htw.saarland.stl.Stdin;
+
+/**
+ *
+ * @author Carsten
+ */
+public class Artikel {
+    
+}
---
+++ uebung01/LagerVerwaltung/src/lagerverwaltung/Lager.java
@@ -0,0 +1,15 @@
+/**
+ * Softwaretechnik 2 
+ * Übung 1
+ * @author Carsten Gross / Moritz Fey
+ */
+
+package lagerverwaltung;
+
+/**
+ *
+ * @author Moritz
+ */
+public class Lager {
+    
+}
