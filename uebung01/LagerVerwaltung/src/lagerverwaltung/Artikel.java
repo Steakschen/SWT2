238c5a758c5f28a58303df1acc6e9b9fc7cecb11
Carsten Gross <carstengross1986@googlemail.com>
2014-05-15 16:21:02 +0200

    Grundgerüst Artikel

--- uebung01/LagerVerwaltung/src/lagerverwaltung/Artikel.java
+++ uebung01/LagerVerwaltung/src/lagerverwaltung/Artikel.java
@@ -12,4 +12,36 @@ package lagerverwaltung;
  */
 public class Artikel {
     
+    private int artikelNr;
+    private String bezeichnung;
+    private int bestand;
+    private double preis;
+
+    public Artikel(int artikelNr, String bezeichnung, int bestand, double preis) {
+        
+        this.artikelNr = artikelNr;
+        this.bezeichnung = bezeichnung;
+        this.bestand = bestand;
+        this.preis = preis;
+    }
+    
+    public Artikel(int artikelNr, String bezeichnung,double preis){
+        this(artikelNr, bezeichnung ,0 ,preis);
+    }
+    
+    public void bucheZugang(int menge){
+        
+    }
+    
+    public void bucheAbgang(int menge){
+        
+    }
+    
+    @Override
+    public String toString() {       
+        return "Artikel: " + artikelNr + " Bezeichnung: "+bezeichnung
+                + " Bestand: " +bestand+" Preis: "+preis;
+    }
+    
+    
 }
