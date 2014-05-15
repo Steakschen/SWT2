0d09b77eb5efd08747f3e192fc7804e4ed378628
Carsten Gross <carstengross1986@googlemail.com>
2014-05-15 16:57:53 +0200

    asserts

--- uebung01/LagerVerwaltung/src/lagerverwaltung/Artikel.java
+++ uebung01/LagerVerwaltung/src/lagerverwaltung/Artikel.java
@@ -11,14 +11,28 @@ package lagerverwaltung;
  * @author Carsten
  */
 public class Artikel {
-    
+ 
     private int artikelNr;
     private String bezeichnung;
     private int bestand;
     private double preis;
 
+    @SuppressWarnings("null")
     public Artikel(int artikelNr, String bezeichnung, int bestand, double preis) {
-        
+       // if (artikelNr < 1000) {
+       //     throw new Exception("Artikelnummer nicht 4 stellig!");
+       // }
+       // if (bezeichnung == null) {
+       //     throw new Exception("Bezeichnung ist Null-Referenz!");
+       // }
+        assert artikelNr < 1000 : "Artikelnummer nicht 4 stellig!"; 
+        assert bezeichnung == null : "Bezeichnung ist Null-Referenz!"; 
+        assert bezeichnung.equals(" ") : "Bezeichnung darf keine Leerzeichen enthalten!";
+        assert bezeichnung.equals("\t") : "Bezeichnung darf keine Tabulatoren enthalten!";
+        assert bezeichnung.equals("\n") : "Bezeichnung darf keine Zeile-Ende-Zeichen enthalten!";
+        assert bezeichnung.equals("\r") : "Bezeichnung darf keine Zeile-Ende-Zeichen enthalten!";
+        assert bestand < 0 : "Bestand ist kleiner als 0!";
+        assert preis < 0 : "Preis ist kleiner als 0!";
         this.artikelNr = artikelNr;
         this.bezeichnung = bezeichnung;
         this.bestand = bestand;
@@ -43,17 +57,22 @@ public class Artikel {
     }
 
     public void setBestand(int bestand) {
+        assert bestand < 0 : "Bestand ist kleiner als 0!";
         this.bestand = bestand;
     }
     public void setPreis(double preis) {
+        assert preis < 0 : "Preis ist kleiner als 0!";
         this.preis = preis;
     }
     
         
     public void bucheZugang(int menge){
+        assert menge < 0 : "Menge ist kleiner als 0!";
         bestand += menge;
     }  
     public void bucheAbgang(int menge){
+        assert menge < 0 : "Menge ist kleiner als 0!";
+        assert (bestand-menge) < 0 : "Bestand wird kleiner als 0!";
         bestand -= menge;
     }   
     @Override
@@ -64,3 +83,10 @@ public class Artikel {
     
     
 }
+
+
+/**
+ * Wieso kein Standard Konstruktor?
+ * Weil wir sonst Objekte haben mit gleicher Artikelnummer und 
+ * diese nicht mehr diefferenzieren können.
+ */
