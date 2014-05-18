73e8b15411fcd853dbd165883b203ffade920650
Carsten Gross <carstengross1986@googlemail.com>
2014-05-18 13:06:00 +0200

    lagerDialog erweiterung

--- uebung01/LagerVerwaltung/src/lagerverwaltung/LagerDialog.java
+++ uebung01/LagerVerwaltung/src/lagerverwaltung/LagerDialog.java
@@ -6,7 +6,6 @@
 
 package lagerverwaltung;
 import de.htw.saarland.stl.Stdin;
-import java.text.DecimalFormat;
 
 /**
  *
@@ -14,6 +13,20 @@ import java.text.DecimalFormat;
  */
 public class LagerDialog {
     
+    /*
+    Lager[] lagerArray;
+    int maxLager;
+    int lagerAnzahl;
+
+    public LagerDialog(Lager[] lagerArray, int maxLager) {  
+        this.maxLager = maxLager;
+        this.lagerAnzahl = 0;
+        this.lagerArray = new Lager[maxLager];
+    }
+     
+    private LagerDialog() {
+    }
+    */
     public static void main(String[] args) {
         Lager meinLager = new Lager("Saarbruecken", 5);
         meinLager.erstelleArtikel(4711, "Kebab", 5, 49.99);
@@ -23,7 +36,65 @@ public class LagerDialog {
         meinLager.erstelleArtikel(5846, "Toaster", 2, 15.99);
         meinLager.entferneArtikel(1254);
         meinLager.aenderePreis(20);
-        System.out.println(meinLager.toString());        
+        System.out.println(meinLager.toString());     
+        
+        //LagerDialog lagerDialog = new LagerDialog();
+        //lagerDialog.start();   
+    }
+    /*
+    private void start() {
+        while (weitermachen()){
+            int menu = printMenu();
+            switch(menu){
+                case 1:
+                    assert lagerAnzahl < (maxLager -1) : "Maximale Anzahl Lager erreicht!";
+                    System.out.println("Lagername: ");
+                    String lagerName = Stdin.readString();
+                    System.out.println("Lageranzahl: ");
+                    int maxLagerAnzahl = Stdin.readInt();
+                    lagerArray[lagerAnzahl] = new Lager(lagerName, maxLagerAnzahl);
+                    lagerAnzahl += 1;
+                    break;
+                case 2:
+                    int artikelNr = Stdin.readInt();
+                    String artikelName = Stdin.readString();
+                    int artikelBestand = Stdin.readInt();
+                    double artikelPreis = Stdin.readDouble();
+                    lagerArray[0].erstelleArtikel(artikelNr, artikelName, artikelBestand, artikelPreis);
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                default:
+                    System.out.println("Default got hit O_o");
+                    break;       
+            }
+        }
     }
     
+    private int printMenu() {
+        System.out.println("\n Menu:\n");
+        System.out.println("1 - Lager anlegen \n");
+        System.out.println("2 - Artikel anlegen\n");
+        System.out.println("3 - Artikel löschen\n");
+        System.out.println("4 - Buche Zugang\n");
+        System.out.println("5 - Buche Abgang\n");
+        System.out.println("6 - aendere Preis\n");
+        return Stdin.readInt();
+    }
+    
+    public boolean weitermachen() {
+        char antwort = ' ';
+        System.out.println("Weitermachen?");
+        while ((antwort != 'j') && (antwort != 'n')) {
+            antwort = Stdin.readChar();
+        }    
+        return antwort == 'j';
+    }
+    */
 }
