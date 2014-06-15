4ebb0eeabddd40ad9ca0d5fd7b792e4a75a65ec3
Mofeywalker <moritz.fey@gmail.com>
2014-06-15 15:33:56 +0200

    Revert "Revert "Klasse CD""
    This reverts commit 96587c8695ccfc7fb54589e4cdadd939839a34fb.

--- uebung01/LagerVerwaltung/src/lagerverwaltung/CD.java
+++ uebung01/LagerVerwaltung/src/lagerverwaltung/CD.java
@@ -10,6 +10,47 @@ package lagerverwaltung;
  *
  * @author Moritz
  */
-public class CD {
+public class CD extends Artikel {
+    
+    private static final String KEIN_INTERPRET  = "\tEs Wurde kein Interpret angegeben!\n";
+    private static final String KEIN_TITEL      = "\tEs wurde kein Titel angegeben!\n";
+    private static final String KEINE_SONGS     = "\tEs wurde keine Anzahl der Songs angegeben\n";
+    
+    private String interpret;
+    private String titel;
+    private int anzahlTitel;
+    
+    public CD(int _artikelNummer, String _bezeichnung, double _preis, String _interpret, String _titel, int _anzahlTitel) throws MyException {
+       super(_artikelNummer, _bezeichnung, _preis);
+       String msg = null;
+       if (_interpret == null || _interpret.trim().length() == 0) {
+           msg += KEIN_INTERPRET;
+       }
+       if (_titel == null || _titel.trim().length() == 0) {
+           msg += KEIN_TITEL;
+       }
+       if (_anzahlTitel <= 0) {
+           msg += KEINE_SONGS;
+       }
+       if (msg != null) {
+           throw new MyException(msg);
+       }
+       else {
+           this.interpret   = _interpret;
+           this.titel       = _titel;
+           this.anzahlTitel = _anzahlTitel;
+       }
+    }
+    
+    public String getBeschreibung() {
+        String beschreibung = new String(interpret + ": " + titel);
+        return beschreibung;
+    }
+    
+    public String toString() {
+        String cdString = super.toString();
+        cdString = cdString + " " + this.getBeschreibung();
+        return cdString;
+    }
     
 }
