3dd8b326ff2f4c370c2b5e6eedf18404154340a6
Mofeywalker <moritz.fey@gmail.com>
2014-06-16 12:45:03 +0200

    Javadoc Kommentare CD

--- uebung01/LagerVerwaltung/src/lagerverwaltung/CD.java
+++ uebung01/LagerVerwaltung/src/lagerverwaltung/CD.java
@@ -7,20 +7,43 @@
 package lagerverwaltung;
 
 /**
- *
+ * Klasse fuer eine CD.
  * @author Moritz
  */
 public class CD extends Artikel {
     
+    /* Konstanten fuer Fehlermeldungen */
     private static final String KEIN_INTERPRET  = "\tEs Wurde kein Interpret angegeben!\n";
     private static final String KEIN_TITEL      = "\tEs wurde kein Titel angegeben!\n";
     private static final String KEINE_SONGS     = "\tEs wurde keine Anzahl der Songs angegeben\n";
     
+    /**
+     * Der Interpret der CD.
+     */
     private String interpret;
+    
+    /**
+     * Der Titel der CD.
+     */
     private String titel;
+    
+    /**
+     * Die Anzahl der Musiktitel auf der CD.
+     */
     private int anzahlTitel;
     
-    public CD(int _artikelNummer, String _bezeichnung, double _preis, String _interpret, String _titel, int _anzahlTitel) throws MyException {
+    /**
+     * Konstruktor fuer eine CD.
+     * @param _artikelNummer    Artikelnummer der CD
+     * @param _bezeichnung      Bezeichnung der CD
+     * @param _preis            Preis der CD
+     * @param _interpret        Interpret der CD
+     * @param _titel            Titel der CD
+     * @param _anzahlTitel      Anzahl der Titel der CD
+     * @throws MyException 
+     */
+    public CD(int _artikelNummer, String _bezeichnung, double _preis, 
+            String _interpret, String _titel, int _anzahlTitel) throws MyException {
        super(_artikelNummer, _bezeichnung, _preis);
        String msg = null;
        if (_interpret == null || _interpret.trim().length() == 0) {
@@ -42,11 +65,43 @@ public class CD extends Artikel {
        }
     }
     
+    /**
+     * Get-Methode fuer die Anzahl der Titel der CD.
+     * @return Anzahl der Titel
+     */
+    public int getAnzahlTitel() {
+        return anzahlTitel;
+    }
+    
+    /**
+     * Get-Methode fuer den Interpreten der CD.
+     * @return Interpret der CD
+     */
+    public String getInterpret() {
+        return interpret;
+    }
+    
+    /**
+     * Get-Methode fuer den Titel der CD.
+     * @return Titel der CD
+     */
+    public String getTitel() {
+        return titel;
+    }
+    
+    /**
+     * Liefert die Beschreibung der CD als String.
+     * @return Beschreibung der CD als String
+     */
     public String getBeschreibung() {
         String beschreibung = new String(interpret + ": " + titel);
         return beschreibung;
     }
     
+    /**
+     * toString() Methode der Klasse CD.
+     * @return 
+     */
     public String toString() {
         String cdString = super.toString();
         cdString = cdString + " " + this.getBeschreibung();
