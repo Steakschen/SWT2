8a64408dc8d7767caad7112112a9af61c6e2bde7
Carsten Gross <carstengross1986@googlemail.com>
2014-06-17 17:57:34 +0200

    Buch/DvD implementiert
    TODO: MWST 7% bei BUCH in der toString

--- uebung01/LagerVerwaltung/src/lagerverwaltung/Buch.java
+++ uebung01/LagerVerwaltung/src/lagerverwaltung/Buch.java
@@ -3,7 +3,6 @@
  * Uebung 1
  * @author Carsten Gross / Moritz Fey
  */
-
 package lagerverwaltung;
 
 /**
@@ -11,14 +10,98 @@ package lagerverwaltung;
  * @author Moritz
  */
 public class Buch extends Artikel {
-    
+
+    /* Konstanten fuer Fehlermeldungen */
+    private static final String KEIN_TITEL
+            = "\tEs wurde kein Titel angegeben!\n";
+    private static final String KEIN_AUTOR
+            = "\tEs wurde kein Autor angegeben!\n";
+    private static final String KEIN_VERLAG
+            = "\tEs wurde kein Verlag angegeben!\n";
+
+    /**
+     * Attribute
+     */
     String titel;
     String autor;
     String verlag;
-    
-    public Buch (int artikelNr, String bezeichnung, int bestand, double preis,
+
+    /**
+     * Konstruktor des Buchs
+     * @param artikelNr
+     * @param bezeichnung
+     * @param bestand
+     * @param preis
+     * @param titel
+     * @param autor
+     * @param verlag
+     * @throws MyException 
+     */
+    public Buch(int artikelNr, String bezeichnung, int bestand, double preis,
             String titel, String autor, String verlag) throws MyException {
+
         super(artikelNr, bezeichnung, bestand, preis);
-        
+
+        String msg = null;
+        if (titel == null || titel.trim().length() == 0) {
+            msg += KEIN_TITEL;
+        }
+        if (autor == null || autor.trim().length() == 0) {
+            msg += KEIN_AUTOR;
+        }
+        if (verlag == null || autor.trim().length() == 0) {
+            msg += KEIN_VERLAG;
+        }
+        if (msg != null) {
+            throw new MyException(msg);
+        } else {
+            this.titel = titel;
+            this.autor = autor;
+            this.verlag = verlag;
+        }
+    }
+    
+    /**
+     * Liefert den Titel des Buchs
+     * @return Buchtitel
+     */
+    public String getTitel() {
+        return titel;
+    }
+
+    /**
+     * Liefert den Autor des Buchs
+     * @return Buchautor
+     */
+    public String getAutor() {
+        return autor;
+    }
+    
+    /**
+     * Liefert den Verlag
+     * @return Verlag des Buchs
+     */
+    public String getVerlag() {
+        return verlag;
+    }
+    
+    /**
+     * Liefert den Autor und Titel des Buchs als String.
+     *
+     * @return Titel der DVD als String
+     */
+    public String getBeschreibung() {
+        String beschreibung = new String(autor + ": " + titel);
+        return beschreibung;
+    }
+
+    /**
+     * toString() Methode der Klasse Buch.
+     * @return
+     */
+    public String toString() {
+        String cdString = super.toString();
+        cdString = cdString + " " + this.getBeschreibung();
+        return cdString;
     }
 }
--- uebung01/LagerVerwaltung/src/lagerverwaltung/DVD.java
+++ uebung01/LagerVerwaltung/src/lagerverwaltung/DVD.java
@@ -3,20 +3,110 @@
  * Uebung 1
  * @author Carsten Gross / Moritz Fey
  */
-
 package lagerverwaltung;
+
+import java.util.Calendar;
 
 /**
  *
  * @author Moritz
  */
 public class DVD extends Artikel {
-    
+
+    /* Konstanten fuer Fehlermeldungen */
+    private static final String KEIN_TITEL
+            = "\tEs wurde kein Titel angegeben!\n";
+    private static final String NEGATIVE_SPIELDAUER
+            = "\tEs wurde eine negative/keine Spieldauer angegeben!\n";
+    private static final String UNGUELTIGES_ERSCHEINUNGSJAHR
+            = "\tEs wurde ein negatives oder zukünftiges Erscheinungsjahr eingegeben!\n";
+
+    /* Attribute */
     String titel;
     double spieldauer;
     int erscheinungsjahr;
-    public DVD(int artikelNr, String bezeichnung, int bestand, double preis, 
-            String titel, double spieldauer, int erscheinungsjahr) throws MyException {
+
+    /* Kalender für das aktuelle Jahr zu prüfen */
+    Calendar cal = Calendar.getInstance();
+    int aktuellesJahr = cal.get(Calendar.YEAR);
+    
+    /**
+     * Konstruktor der DVD
+     *
+     * @param artikelNr
+     * @param bezeichnung
+     * @param bestand
+     * @param preis
+     * @param titel
+     * @param spieldauer
+     * @param erscheinungsjahr
+     * @throws MyException
+     */
+    public DVD(int artikelNr, String bezeichnung, int bestand, double preis,
+            String titel, double spieldauer, int erscheinungsjahr)
+            throws MyException {
+
         super(artikelNr, bezeichnung, bestand, preis);
+
+        String msg = null;
+        if (titel == null || titel.trim().length() == 0) {
+            msg += KEIN_TITEL;
+        }
+        if (spieldauer <= 0) {
+            msg += NEGATIVE_SPIELDAUER;
+        }
+        if (erscheinungsjahr < 0 || erscheinungsjahr > aktuellesJahr) {
+            msg += UNGUELTIGES_ERSCHEINUNGSJAHR;
+        }
+        if (msg != null) {
+            throw new MyException(msg);
+        } else {
+            this.titel = titel;
+            this.spieldauer = spieldauer;
+            this.erscheinungsjahr = erscheinungsjahr;
+        }
+    }
+
+    /**
+     * Liefert den Titel der DVD
+     * @return DVD Titel
+     */
+    public String getTitel() {
+        return titel;
+    }
+
+    /**
+     * Liefert die Spieldauer der DVD
+     * @return Spieldauer der DVD
+     */
+    public double getSpieldauer() {
+        return spieldauer;
+    }
+
+    /**
+     * Liefert das Erscheinungsjahr der DVD
+     * @return Erscheinungsjahr der DVD
+     */
+    public int getErscheinungsjahr() {
+        return erscheinungsjahr;
+    }
+
+    /**
+     * Liefert den Titel der DVD als String.
+     * @return Titel der DVD als String
+     */
+    public String getBeschreibung() {
+        String beschreibung = new String(titel);
+        return beschreibung;
+    }
+    
+    /**
+     * toString() Methode der Klasse DVD.
+     * @return 
+     */
+    public String toString() {
+        String cdString = super.toString();
+        cdString = cdString + " " + this.getBeschreibung();
+        return cdString;
     }
 }
