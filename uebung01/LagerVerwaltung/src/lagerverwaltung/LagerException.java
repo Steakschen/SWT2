a9779037251485910edfc1880e45c2c895aa13c1
Mofeywalker <moritz.fey@gmail.com>
2014-06-25 16:26:12 +0200

    Klasse LagerException angelegt

---
+++ uebung01/LagerVerwaltung/src/lagerverwaltung/LagerException.java
@@ -0,0 +1,17 @@
+/*
+ * Softwaretechnik 2 
+ * Uebung 1
+ * @author Carsten Gross / Moritz Fey
+ */
+
+package lagerverwaltung;
+
+/**
+ *
+ * @author Moritz
+ */
+public class LagerException extends Exception {
+   public LagerException(String _msg) {
+       super(_msg);
+   }
+}
