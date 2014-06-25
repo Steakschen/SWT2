51ad416d5ac606dd498de400e7742be2b2312474
Mofeywalker <moritz.fey@gmail.com>
2014-06-25 16:26:28 +0200

    Klasse ArtikelException angelegt

---
+++ uebung01/LagerVerwaltung/src/lagerverwaltung/ArtikelException.java
@@ -0,0 +1,17 @@
+/*
+ * Softwaretechnik 2 
+ * Uebung 1
+ * @author Carsten Gross / Moritz Fey
+ */
+
+package lagerverwaltung;
+
+/**
+ *
+ * @author Moritz
+ */
+public class ArtikelException extends Exception {
+    public ArtikelException(String _msg) {
+        super(_msg);
+    }
+}
