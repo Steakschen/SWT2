3224832edd101cc3c3235bc0ffd3863139fe90b7
Mofeywalker <moritz.fey@gmail.com>
2014-07-01 15:02:09 +0200

    Hässlich, aber ... hässlich

--- uebung01/LagerVerwaltung/src/lagerverwaltung/LagerGuiMain.java
+++ uebung01/LagerVerwaltung/src/lagerverwaltung/LagerGuiMain.java
@@ -6,7 +6,11 @@
 
 package lagerverwaltung;
 
-import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
 
 /**
  *
@@ -17,6 +21,46 @@ public class LagerGuiMain extends JFrame {
         setTitle("Lagerverwaltung");
         setSize(550, 400);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel lagerOrt         = new JLabel("Lagerort");
+        JPanel buttonFeld       = new JPanel(new FlowLayout());
+        JButton neuButton       = new JButton("neu");
+        JButton loeschenButton  = new JButton("loeschen");
+        JButton zugangButton    = new JButton("Zugang");
+        JButton abgangButton    = new JButton("Abgang");
+        JTextArea textFeld      = new JTextArea("Test");
+        JScrollPane scrollPane  = new JScrollPane(textFeld);
+        add(scrollPane, BorderLayout.CENTER);
+        add(lagerOrt, BorderLayout.NORTH);
+        buttonFeld.add(neuButton);
+        buttonFeld.add(loeschenButton);
+        buttonFeld.add(zugangButton);
+        buttonFeld.add(abgangButton);
+        add(buttonFeld, BorderLayout.SOUTH);
+        
+        neuButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //neuen Artikel anlegen
+            }
+        });
+        
+        loeschenButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //Artikel loeschen
+            }
+        });
+        
+        zugangButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //Zugang buchen
+            }
+        });
+        
+        abgangButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                //Abgang buchen
+            }
+        });
+        
     }
     
     public static void main(String[] args) {
