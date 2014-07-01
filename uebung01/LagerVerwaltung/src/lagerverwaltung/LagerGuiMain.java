55b7ef6fb780b024afc53ca705c78305ceaa45a8
Mofeywalker <moritz.fey@gmail.com>
2014-07-01 14:21:13 +0200

    Klasse für Gui

---
+++ uebung01/LagerVerwaltung/src/lagerverwaltung/LagerGuiMain.java
@@ -0,0 +1,17 @@
+/*
+ * Softwaretechnik 2 
+ * Uebung 5
+ * @author Carsten Gross / Moritz Fey
+ */
+
+package lagerverwaltung;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Moritz
+ */
+public class LagerGuiMain extends JFrame {
+    
+}
